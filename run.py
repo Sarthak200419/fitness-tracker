8d13b863b33687e8b9233935eafa207677638a6f
Sarthak <sarthak1212s@gmail.com>
2025-12-11 12:05:17 +0530

    added commands

--- run.py
+++ run.py
@@ -7,3 +7,18 @@ app = create_app(os.environ.get('FLASK_ENV', 'development'))
 if __name__ == '__main__':
     # Run in HTTP mode (reverted from HTTPS)
     app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=5000)
+
+
+
+"""
+python -m venv venv
+venv\Scripts\activate  # On Windows
+# or
+source venv/bin/activate  # On Mac/Linux
+
+Install dependencies
+pip install -r requirements.txt
+
+Then Run
+python run.py
+"""
